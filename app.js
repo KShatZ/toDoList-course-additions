944735d9c0747638a6551864d3cd2094aafefabd
Kirill <cierio.dev@gmail.com>
2021-12-23 15:51:14 -0500

    Develop basic add and remove functionality from course

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const date = require(__dirname + "/modules/date.js");
 
-
-
 const app = express();
 
 //Middleware Set-up
@@ -12,8 +10,26 @@ app.use(express.static("public")); //Serve static files from 'public' directory
 //Setting EJS as view engine
 app.set("view engine", "ejs"); 
 
+
+const items = []; //Array to hold ToDo items
+
+
+// '/' routes
 app.get("/", function(req, res){
-    res.render("list", {pageTitle: "Title"});
+   
+    res.render("list", {
+        pageTitle: "Title", 
+        day:date.getDate(),
+        items:items
+    });
+
+});
+
+app.post("/", function (req, res){
+
+    items.push(req.body.newItem);
+    res.redirect("/");
+
 });
 
 app.listen("3000", function(req, res){
